bcff2b832496af4f5c9e85606f877cbf17657f96
Rizky Subagja <rizkysubagja99@gmail.com>
2025-02-14 00:48:36 +0700

    implement flat list to render list of data

--- src/screens/PokemonListScreen/PokemonListScreen.tsx
+++ src/screens/PokemonListScreen/PokemonListScreen.tsx
@@ -1,17 +1,29 @@
-import { View } from 'react-native';
+import { FlatList, ListRenderItemInfo, View } from 'react-native';
 
 import CardViewComponent from '../../components/CardViewComponent';
 import styles from './PokemonListScreen.style';
+import PokemonData from '@/src/fixtures/PokemonData';
+import Pokemon from '@/src/interfaces/PokemonInterface';
+
+const { pokemonList } = PokemonData;
 
 const PokemonListScreen = () => {
-  return (
-    <>
+  const renderPokemonCard = (pokemon: Pokemon) => {
+    return(
       <View style={styles.container}>
-        <CardViewComponent />
-        <View style={styles.cardContainer}>
-        </View>
+        <CardViewComponent pokemon={pokemon} />
       </View>
-    </>
+    )
+  }
+  
+  return (
+    <View>
+      <FlatList 
+        data={pokemonList}
+        numColumns={2}
+        renderItem={({item}) => renderPokemonCard(item)}
+      />
+    </View>
   );
 }
 
